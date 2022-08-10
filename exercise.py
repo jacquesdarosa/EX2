7da505c045318e8aa37d0e8bc5af1e04874ad7db
Jacques <jacquesdarosa@gmail.com>
2022-08-10 09:34:29 +0200

    Change_1_EX2

---
+++ exercise.py
@@ -0,0 +1,32 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+
+# task 1
+
+
+a= 2/3
+print(round(a, 3)) # 3 stands for decimals
+print(12+34)
+print(43-56)
+print(4*4)
+print(4/5)
+print(12%5)
+print (12.2//3)
+print(12.5//3)
+print(True*False)
+print(5*(2-3j))
+
+
+# Task 2
+print(max(2, 2.0, -0.5))
+print(min(23, 22, 55.0))
+print(abs(-34.23))
+print(pow(3, 4))
+
+import math
+print(math.ceil(34/5))
+print(math.floor(34/5))
+print(max(True, False))
+print(abs(4-3j))
